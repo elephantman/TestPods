da4a87a9280735d4220cd8e5330b135b70b38c37
David House <davidahouse@gmail.com>
2013-09-22 11:23:50 -0400

    initial commit

---
+++ TestPods/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestPods
+//
+//  Created by David House on 9/22/13.
+//  Copyright (c) 2013 KP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestPods/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestPods
+//
+//  Created by David House on 9/22/13.
+//  Copyright (c) 2013 KP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
